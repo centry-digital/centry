10c09734c4b265d176c7f5ba56f60fe576d2ca99
YL Chow <ylchow@YLs-M1-MacBook-Pro.local>
2022-04-27 20:33:38 +0800

    added shareholder.js

---
+++ incorporation/shareholder.js
@@ -0,0 +1,15 @@
+const radioButtons = document.querySelectorAll('input[name="Shareholder-Type"]');
+const radioDivs = document.querySelectorAll(".w-radio");
+for (const radioDiv of radioDivs) {
+	radioDiv.setAttribute("onclick", "selectRadio()");	
+}
+function selectRadio() {
+	console.log("eventListener triggered")
+  for (let i = 0; i < radioButtons.length; i++) {
+    if (radioButtons[i].checked) {
+    	radioDivs[i].classList.add("active");
+    } else {
+    	radioDivs[i].classList.remove("active");
+    }
+  }
+}
