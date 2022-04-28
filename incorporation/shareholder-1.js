dcc5329e6e3cd44bea44214d2b156e95690f23a2
YL Chow <ylchow@YLs-M1-MacBook-Pro.local>
2022-04-28 14:03:19 +0800

    Added function to calculate number of shares. Save file under new name - shareholder-1.js

---
+++ incorporation/shareholder-1.js
@@ -0,0 +1,42 @@
+//switching between individual and corporate shareholder type
+const radioButtons = document.querySelectorAll('input[name="Shareholder-Type"]');
+const radioDivs = document.querySelectorAll(".w-radio");
+const individualShareholderGroup = document.querySelector('div[data-shareholder-group="individual"]');
+const corporateShareholderGroup = document.querySelector('div[data-shareholder-group="corporate"]');
+
+for (const radioDiv of radioDivs) {
+	radioDiv.style.cursor = "pointer";
+  radioDiv.setAttribute("onclick", "selectRadio()");	
+}
+
+function selectRadio() {
+  for (let i = 0; i < radioButtons.length; i++) {
+    let shareholderType;
+    if (radioButtons[i].checked) {
+    	radioDivs[i].classList.add("active");
+      shareholderType = radioDivs[i].getAttribute("data-shareholder-type");
+      if (shareholderType == "individual") {
+      	individualShareholderGroup.classList.remove("hide")
+        corporateShareholderGroup.classList.add("hide")
+      } else if (shareholderType == "corporate") {
+      	corporateShareholderGroup.classList.remove("hide")
+        individualShareholderGroup.classList.add("hide")
+      }
+    } else {
+    	radioDivs[i].classList.remove("active");
+    }
+  }
+}
+
+//calculate number of shares
+const numberOfShares = 1000;
+const sharesPercent = document.querySelectorAll('input[data-shareholding="percent"]');
+const sharesNumber = document.querySelectorAll('span[data-shareholding="number"]');
+for (let i = 0; i < sharesPercent.length; i++) {
+  sharesPercent[i].addEventListener("keyup", function() {
+  	percentage = sharesPercent[i].value;
+  	percentToNumber = Math.round(percentage);
+  	sharesNumber[i].innerText = percentToNumber;
+		}
+ 	);
+}
