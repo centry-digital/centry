2bb0c6729799962318e4e197bec85766e2ba799b
YL Chow <ylchow@YLs-M1-MacBook-Pro.local>
2022-05-06 00:39:15 +0800

    bug fixes

--- incorporation/shareholder-3a.js
+++ incorporation/shareholder-3a.js
@@ -284,10 +284,10 @@ function selectRadio(el) {
 }
   
 //calculate number of shares
+let totalShares = 0;
 function calculateShares() {
   sharesPercent = document.querySelectorAll('input[data-shareholding="percent"]');
   sharesNumber = document.querySelectorAll('span[data-shareholding="number"]');
-  let totalShares = 0;
   for (let i = 0; i < sharesPercent.length; i++) {
     sharesPercent[i].addEventListener("keyup", function() {
       percentage = sharesPercent[i].value;
