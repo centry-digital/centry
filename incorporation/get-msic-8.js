f7615dd4abd2bf605afc6f7a21d10af2257ea641
YL Chow <ylchow@YLs-M1-MacBook-Pro.local>
2022-05-10 14:13:11 +0800

    bug fixes

--- incorporation/get-msic-8.js
+++ incorporation/get-msic-8.js
@@ -14,13 +14,13 @@ function getMsicCodes() {
 				incorporationSummary.msicCodes = [];
         for (const selection of selections){
 					incorporationSummary.msicCodes.push(selection.value);
-					if (incorporationSummary.msicCodes == "") {
-						console.log("msic code is empty");
-						document.querySelector(".ss-multi-selected").classList.add("invalid-field");
-					} else {
-						document.querySelector(".ss-multi-selected").classList.remove("invalid-field");
-						console.log("msic code is not empty");
-					}
+				}
+				if (incorporationSummary.msicCodes != "") {
+					console.log("msic code is empty");
+					document.querySelector(".ss-multi-selected").classList.remove("invalid-field");
+				} else {
+					document.querySelector(".ss-multi-selected").classList.add("invalid-field");
+					console.log("msic code is not empty");
 				}
 		  }
 		})
