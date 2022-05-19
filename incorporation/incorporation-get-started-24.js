9e5aae9bccfbf24d990a8bffd0108bcf52a04230
YL Chow <yl@centry.digital>
2022-05-19 19:18:59 +0800

    bug fixes

--- incorporation/incorporation-get-started-24.js
+++ incorporation/incorporation-get-started-24.js
@@ -15,8 +15,7 @@ retrieveEmail.addEventListener("keyup", validateEmail);
 uniqueCodeInput.addEventListener("keyup", validateCapitaliseCode);
 continueIncorporatingBtn.addEventListener("click", continueIncorporating);
 
-retrieveBtn.innerHTML =
-  '<svg xmlns="http://www.w3.org/2000/svg" width="28px" height="28px" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5"><path stroke-linecap="round" stroke-linejoin="round" d="M17 8l4 4m0 0l-4 4m4-4H3"></path></svg>';
+// retrieveBtn.innerHTML ='<svg xmlns="http://www.w3.org/2000/svg" width="28px" height="28px" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5"><path stroke-linecap="round" stroke-linejoin="round" d="M17 8l4 4m0 0l-4 4m4-4H3"></path></svg>';
 retrieveBtn.style.cursor = "not-allowed";
 retrieveForm.addEventListener("keypress", function (event) {
   if (event.key == "Enter") {
