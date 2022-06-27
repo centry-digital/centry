7ac0faf6f17c6a98b8054edeab7b924fb6aafcc0
YL Chow <yl@centry.digital>
2022-06-28 00:39:58 +0800

    bug fixes

--- incorporation/e-kyc-13.js
+++ incorporation/e-kyc-13.js
@@ -68,7 +68,7 @@ async function retrieveUser(token) {
       inputData = data;
       if (inputData.response.verified == "false") {
         document.getElementById("w-tabs-0-data-w-tab-0").click();
-        setTimeout(() => tabs.classList.remove("hide"), 500);
+        setTimeout(() => tabs.classList.remove("hide"), 100);
         uuid = inputData.response.uuid;
         inputFname.value = inputData.response.first_name;
         inputLname.value = inputData.response.last_name;
@@ -82,7 +82,7 @@ async function retrieveUser(token) {
         ind_shareholder_uuid = inputData.ind_shareholder_uuid;
       } else if (inputData.response.verified == "pending") {
         document.getElementById("w-tabs-0-data-w-tab-2").click();
-        setTimeout(() => tabs.classList.remove("hide"), 500);
+        setTimeout(() => tabs.classList.remove("hide"), 100);
         let resumeBtn = document.querySelector(
           '[data-kyc="button-resume-verification"]'
         );
@@ -94,7 +94,7 @@ async function retrieveUser(token) {
         });
       } else if (inputData.response.verified == "true") {
         document.getElementById("w-tabs-0-data-w-tab-3").click();
-        setTimeout(() => tabs.classList.remove("hide"), 500);
+        setTimeout(() => tabs.classList.remove("hide"), 100);
       }
     } else {
       console.error("There is an error retrieving the user to verify");
