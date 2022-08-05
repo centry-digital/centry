bb0529ba3c4a2d2beecb755bf197e058f2c00f63
YL Chow <yl@centry.digital>
2022-08-05 12:42:05 +0800

    added headers to submit form data to allow for API calls to be made

--- e-kyc/post-submission.js
+++ e-kyc/post-submission.js
@@ -1,36 +1,48 @@
 let verification_object = {};
 let sessionToken = sessionStorage.getItem("token");
 const query = new URLSearchParams(window.location.search);
-if (query.has("verification") && query.has("token") && query.has("manual") && query.has("fname") && query.has("lname") && query.has("dob") && query.has("nationality")) {
+if (
+  query.has("verification") &&
+  query.has("token") &&
+  query.has("manual") &&
+  query.has("fname") &&
+  query.has("lname") &&
+  query.has("dob") &&
+  query.has("nationality")
+) {
   if (query.get("manual")) {
-  	verification_object.verification_uuid = query.get("verification");
+    verification_object.verification_uuid = query.get("verification");
     verification_object.manual_verification = query.get("manual");
     verification_object.first_name = query.get("fname");
     verification_object.last_name = query.get("lname");
     verification_object.dob = query.get("dob");
     verification_object.nationality = query.get("nationality");
     submitVerification(sessionToken);
-    
+
     document.getElementById("e-kyc-container").classList.add("hide");
     document.getElementById("manual-container").classList.remove("hide");
   } else {
-  	document.getElementById("e-kyc-container").classList.remove("hide");
+    document.getElementById("e-kyc-container").classList.remove("hide");
     document.getElementById("manual-container").classList.add("hide");
-  };
+  }
 }
 
 async function submitVerification(token) {
-	try {
-  	let response = await fetch("https://api.centry.digital/api:ekyc/verify/submit",
+  try {
+    let response = await fetch(
+      "https://api.centry.digital/api:ekyc/verify/submit",
       {
         method: "POST",
         headers: {
-          "Auth": "Bearer " + token,
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+          "Access-Control-Allow-Headers": "Authorization",
+          Auth: "Bearer " + token,
         },
-        body: JSON.stringify(verification_object)
+        body: JSON.stringify(verification_object),
       }
-  	);
+    );
   } catch (error) {
-  	console.error(error)
+    console.error(error);
   }
 }
